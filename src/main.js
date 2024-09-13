36843a2290785cea4d4a1db98470267603e9585d
Keshav Madhav <keshav2552003@gmail.com>
2024-09-13 11:04:36 +0530

    Added terrain classes and created a plane

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import Stats from 'three/examples/jsm/libs/stats.module.js';
 import GUI from 'three/examples/jsm/libs/lil-gui.module.min.js';
+import { Terrain } from './terrain';
 
 const gui = new GUI();
 
@@ -14,6 +15,9 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const controls = new OrbitControls( camera, renderer.domElement );
 
+const terrain = new Terrain(10, 10);
+scene.add(terrain);
+
 const sun = new THREE.DirectionalLight();
 sun.position.set( 1, 2, 3 );
 scene.add( sun );
@@ -21,11 +25,6 @@ scene.add( sun );
 const ambientLight = new THREE.AmbientLight();
 ambientLight.intensity = 0.5;
 scene.add( ambientLight );
-
-const geometry = new THREE.BoxGeometry( 1, 2, 3 );
-const material = new THREE.MeshStandardMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
 
 const stats = new Stats();
 document.body.appendChild( stats.dom );
@@ -45,8 +44,10 @@ window.addEventListener( 'resize', () => {
 	renderer.setSize( window.innerWidth, window.innerHeight );
 })
 
-const folder = gui.addFolder('Cube')
-folder.add(cube.position, 'x', -10, 10).name('position x');
-folder.add(cube.position, 'y', -10, 10).name('position y');
-folder.add(cube.position, 'z', -10, 10).name('position z');
-folder.addColor(material, 'color').name('color');
+const terrainFolder = gui.addFolder('Terrain');
+terrainFolder.addColor(terrain.material, 'color').name('Color');
+terrainFolder.add(terrain, 'width', 1, 100).name('Width');
+terrainFolder.add(terrain, 'height', 1, 100).name('Height');
+terrainFolder.onChange(() => {
+	terrain.createGeometry();
+})
